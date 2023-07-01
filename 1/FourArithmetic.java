39e8c2a7d35fe72d5b04b70694e6347de0a25933
John Byeon <qusdudwns0162@gmail.com>
2023-07-01 23:40:52 +0900

    Add files via upload

---
+++ 1/FourArithmetic.java
@@ -0,0 +1,10 @@
+public class FourArithmetic {
+    public static void main(String[] args){
+        //두 개의 정수를 사칙연산하여 출력하는 JavaSE프로그램 만들어보기
+        int a = 12; int b = 2;
+        System.out.println("a+b="+(a+b)); //'+' = Concatenation 기호
+        System.out.println("a-b="+(a-b));
+        System.out.println("a*b="+(a*b));
+        System.out.println("a/b="+(a/b));
+    }
+}
