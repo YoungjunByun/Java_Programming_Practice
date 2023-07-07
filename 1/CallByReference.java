eb25fbd79b5931d203b0ad0ba73bc1562bed7e2c
Jun Byeon <qusdudwns0162@gmail.com>
2023-07-07 18:31:20 +0900

    Add files via upload

---
+++ 1/CallByReference.java
@@ -0,0 +1,13 @@
+public class CallByReference {
+    public static void main(String[] args) {
+            float a = 56.7f;
+            float b = 78.9f;
+            floatAdd(a, b); //Call By Value
+        System.out.println("종료");
+    }
+    //매개변수로 2개의 실수를 받아서 총합을 구하여 출력하는 메서드를 정의하세요.
+    public static void floatAdd(float a, float b){
+        float sum = a + b;
+        System.out.println("sum = " + sum);
+    }
+}
---
+++ 1/MethodMakeTest.java
@@ -0,0 +1,12 @@
+public class MethodMakeTest {
+    public static void main(String[] args) {
+        int a = 3, b = 5;
+        int result = add(a, b);
+        System.out.println("result = " + result);
+    }
+    //매개변수로 2개의 정수를 받아서 총합을 구하여 리턴하는 메서드를 정의하라
+    public static int add(int a, int b){
+        int sum = a + b;
+        return sum;
+    }
+}
